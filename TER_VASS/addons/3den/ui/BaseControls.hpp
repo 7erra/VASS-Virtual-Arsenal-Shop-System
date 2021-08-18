96e12d002fff609da9393e8a195acb0887f30cb3
Terra <ichbinterra@gmail.com>
2021-08-18 13:39:35 +0200

    base class for controls table

--- TER_VASS/addons/3den/ui/BaseControls.hpp
+++ TER_VASS/addons/3den/ui/BaseControls.hpp
@@ -1,3 +1,4 @@
+class ctrlDefault;
 class ctrlButton;
 class ctrlListNBox;
 class ctrlStatic;
@@ -15,3 +16,29 @@ class ctrlStaticTitle;
 class ctrlButtonCancel;
 class ctrlStaticFooter;
 class ctrlButtonOK;
+class ctrlControlsGroupNoScrollbars;
+class ctrlCheckbox;
+class ctrlControlsTable: ctrlControlsGroup
+{
+	idc = -1;
+	type = CT_CONTROLS_TABLE;
+	style = SL_TEXTURES;
+
+	lineSpacing = 0;
+	rowHeight = 5 * GRID_H;
+	headerHeight = 5 * GRID_H;
+	firstIDC = 100;
+	lastIDC = 999;
+	// Colours which are used for animation (i.e. change of colour) of the selected line.
+	selectedRowColorFrom[]  = {0.7, 0.85, 1, 0.25};
+	selectedRowColorTo[]	= {0.7, 0.85, 1, 0.5};
+	// Length of the animation cycle in seconds.
+	selectedRowAnimLength = 1.2;
+	// Template for selectable rows
+	class RowTemplate
+	{
+	};
+	class HeaderTemplate
+	{
+	};
+};
