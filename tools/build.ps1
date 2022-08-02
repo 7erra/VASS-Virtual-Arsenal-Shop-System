779f5f54fb5c7f6622d8ca99ee7030c72a68f676
7erra <38866692+7erra@users.noreply.github.com>
2022-08-02 16:20:46 +0200

    Added build script

---
+++ tools/build.ps1
@@ -0,0 +1,4 @@
+﻿$tools_dir = Get-Item $PSScriptRoot # Path to this file
+$vass_dir = Get-Item $tools_dir.PSParentPath # VASS root is one directory above the tools directory
+Set-Location -Path $vass_dir\TER_VASS
+hemtt.exe build
